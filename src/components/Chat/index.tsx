c7deed399ff1606ab125396ecb109bf43c0b038a
trevor-shepard <trevorbshepard@gmail.com>
2020-08-18 15:43:06 -0700

    Chat page loading mechanics

--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -3,7 +3,8 @@ import { useHistory, useLocation } from 'react-router-dom'
 import { backArrow } from 'assets/icons'
 import { useSelector, useDispatch } from 'react-redux'
 import { RootState } from 'store/rootReducer'
-import { recieveChat } from 'store/slices/chatsSlice'
+import { recieveChat, DatabaseChat } from 'store/slices/chatsSlice'
+import { convertTimestamp } from 'utils/dateUtils'
 import { db } from 'utils/firebase'
 
 interface LocationState {
@@ -14,23 +15,43 @@ interface LocationState {
 
 const Chat: FunctionComponent = () => {
 	const dispatch = useDispatch()
-
 	// do not render if no id provided
 	const history = useHistory()
 	const { state } = useLocation() as LocationState
 	if (!state || !state.id) history.goBack()
+
 	const { id } = state
+
 	const chat = useSelector((state: RootState) => state.chats[id])
 
-	const [loading, setLoading] = useState(true)
+	const [loading, setLoading] = useState(chat !== undefined)
 
-	useEffect(() => {
+	useEffect(() =>  {
 		const unsubscribe = db
-			.collection('chats')
-			.doc(id)
-			.onSnapshot(doc => dispatch(doc.data()))
-		return unsubscribe
-	}, [dispatch, id])
+		.collection('chats')
+		.doc(id)
+		.onSnapshot(doc => {
+			const value = doc.data() as DatabaseChat
+				const participants = Object.keys(value.participants).reduce(
+					(acc, id) => {
+						return {
+							...acc,
+							[id]: convertTimestamp(value.participants[id])
+						}
+					},
+					{}
+				)
+
+			dispatch(recieveChat({
+				...value,
+				participants
+			}))
+		})
+
+	return unsubscribe
+	}, [])
+
+	
 
 	return <div></div>
 }
