842046de78875e6a55859e210955ead5ce584601
trevor-shepard <trevorbshepard@gmail.com>
2020-09-02 19:55:50 -0700

    expert list styling

--- src/components/experts/ExpertsDisplay/index.tsx
+++ src/components/experts/ExpertsDisplay/index.tsx
@@ -8,6 +8,9 @@ interface ExpertsDisplayProps {
 }
 
 const Feature: FunctionComponent<ExpertsDisplayProps> = ({ experts }) => {
+
+	if (experts.length === 0) return <EmptyMessage>You haven’t added any experts to your business yet.</EmptyMessage>
+
 	return (
 		<ExpertsContainer>
 			{experts.slice(0, 3).map((expert, i) => (
@@ -23,6 +26,18 @@ const ExpertsContainer = styled.div`
 	justify-content: space-around;
 	align-items: center;
 	width: 100%;
+`
+const EmptyMessage = styled.div`
+	font-family: Open Sans;
+	font-style: normal;
+	font-weight: normal;
+	font-size: 16px;
+	line-height: 22px;
+	/* or 137% */
+
+	letter-spacing: -0.408px;
+
+	color: #696868;
 `
 
 export default Feature
